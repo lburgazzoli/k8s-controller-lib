215a22632ced2ee77d1c4bd9f9f1067cad27a042
Luca Burgazzoli <lburgazzoli@gmail.com>
2024-09-06 16:37:00 +0200

    reconciler

--- reconciler/reconciler.go
+++ reconciler/reconciler.go
@@ -1,1 +1,56 @@
 package reconciler
+
+import (
+	"context"
+	"github.com/lburgazzoli/k8s-controller-lib/client"
+	"reflect"
+
+	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	ctrlClient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type Finalizable interface {
+	Finalize(ctx context.Context, obj ctrlClient.Object) error
+}
+
+type BaseReconciler[T ctrlClient.Object] struct {
+	Log           logr.Logger
+	FinalizerName string
+	Delegate      reconcile.ObjectReconciler[T]
+	Client        *client.Client
+}
+
+//nolint:forcetypeassert,wrapcheck,nestif
+func (s *BaseReconciler[T]) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	res := reflect.New(reflect.TypeOf(*new(T)).Elem()).Interface().(T)
+	if err := s.Client.Get(ctx, req.NamespacedName, res); err != nil {
+		return ctrl.Result{}, ctrlClient.IgnoreNotFound(err)
+	}
+
+	if res.GetDeletionTimestamp().IsZero() {
+		err := AddFinalizer(ctx, s.Client, res, s.FinalizerName)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	} else {
+		f, ok := s.Delegate.(Finalizable)
+		if ok {
+			err := f.Finalize(ctx, res)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+
+		err := RemoveFinalizer(ctx, s.Client, res, s.FinalizerName)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		return ctrl.Result{}, nil
+	}
+
+	return s.Delegate.Reconcile(ctx, res)
+}
--- reconciler/reconciler_support.go
+++ reconciler/reconciler_support.go
@@ -1,1 +1,52 @@
 package reconciler
+
+import (
+	"context"
+	"fmt"
+
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	ctrlClient "sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+func AddFinalizer(ctx context.Context, client ctrlClient.Client, o ctrlClient.Object, name string) error {
+	if name == "" {
+		return nil
+	}
+
+	if !ctrlutil.AddFinalizer(o, name) {
+		return nil
+	}
+
+	err := client.Update(ctx, o)
+	if k8serrors.IsConflict(err) {
+		return fmt.Errorf("conflict when adding finalizer to %s/%s: %w", o.GetNamespace(), o.GetName(), err)
+	}
+
+	if err != nil {
+		return fmt.Errorf("failure adding finalizer to %s/%s: %w", o.GetNamespace(), o.GetName(), err)
+	}
+
+	return nil
+}
+
+func RemoveFinalizer(ctx context.Context, client ctrlClient.Client, o ctrlClient.Object, name string) error {
+	if name == "" {
+		return nil
+	}
+
+	if !ctrlutil.RemoveFinalizer(o, name) {
+		return nil
+	}
+
+	err := client.Update(ctx, o)
+	if k8serrors.IsConflict(err) {
+		return fmt.Errorf("conflict when removing finalizer to %s/%s: %w", o.GetNamespace(), o.GetName(), err)
+	}
+
+	if err != nil {
+		return fmt.Errorf("failure removing finalizer from %s/%s: %w", o.GetNamespace(), o.GetName(), err)
+	}
+
+	return nil
+}
